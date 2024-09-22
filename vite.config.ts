d1d2e997a7259f056654ccab2808283f3db84adb
Yulong Cai <yumoucyl@gmail.com>
2024-09-22 20:10:01 +1000

    set env variable || add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+import path from "path";
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
