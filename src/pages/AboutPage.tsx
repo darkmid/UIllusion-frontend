b2e7dcd89470c51301814af34c55245c528946b8
Yulong Cai <yumoucyl@gmail.com>
2024-10-11 16:06:39 +0800

    Create AboutPage.tsx

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+export default function Home() {
+  return (
+    <main>
+      <Spline scene="https://prod.spline.design/sQKfmqrzqBvzfjX4/scene.splinecode" />
+    </main>
+  );
+}
